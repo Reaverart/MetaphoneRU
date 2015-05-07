cc33325319e8cd6368ad2a0e674c86def3a81c09
Arthur Charaev <reaverart@gmail.com>
2015-05-08 02:13:11 +0300

    add umd

---
+++ metaphone.js
@@ -0,0 +1,12 @@
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define([], factory);
+    } else if (typeof exports === 'object') {
+        module.exports = factory();
+    } else {
+        root.metaphoneRu = factory();
+  }
+}(this, function () {
+
+    return {};
+}));

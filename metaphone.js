ef2056f25fb0a07a2c88c4d9788dc397eef51076
Arthur Charaev <reaverart@gmail.com>
2015-05-08 03:14:36 +0300

    add vowel transform

--- metaphone.js
+++ metaphone.js
@@ -1,3 +1,18 @@
+/*
+    Для всех гласных букв проделать следующие операции.
+    ЙО, ИО, ЙЕ, ИЕ → И
+    О, Ы, Я → А
+    Е, Ё, Э → И
+    Ю → У
+    Для всех согласных букв, за которыми следует любая согласная, кроме Л, М, Н или Р, либо же для согласных на конце слова, провести оглушение:
+    Б → П
+    З → С
+    Д → Т
+    В → Ф
+    Г → К
+    Склеиваем ТС и ДС в Ц:
+    ТС → Ц
+*/
 (function (root, factory) {
     if (typeof define === 'function' && define.amd) {
         define([], factory);
@@ -7,6 +22,31 @@
         root.metaphoneRu = factory();
   }
 }(this, function () {
+    function IOIEtoI (token) {
+        return token.replace(/ЙО|ИО|ЙЕ|ИЕ/g, 'И')
+    }
+    
+    function OIAtoA (token) {
+        return token.replace(/О|Ы|Я/g, 'А')
+    }
 
-    return {};
+    function EtoI (token) {
+        return token.replace(/Е|Ё|Э/g, 'И')
+    }
+
+    function UtoY (token) {
+        return token.replace(/Ю/g, 'У')
+    }
+    return {
+        code: function (token) {
+            token = token.toUpperCase();
+
+            token = IOIEtoI(token);
+            token = OIAtoA(token);
+            token = EtoI(token);
+            token = UtoY(token);
+
+            return token;
+        }
+    };
 }));
